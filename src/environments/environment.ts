5608e99445566e68f6b3788dd7f914573a58c92e
romanov2406 <rmnov2406@gmail.com>
2021-02-12 15:10:52 +0200

    first message

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,9 +2,24 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDJaXuMZH8SmmMBzCPXBCdzRFpvNgWQ5hU',
+    authDomain: 'final-project-romanov.firebaseapp.com',
+    projectId: 'final-project-romanov',
+    storageBucket: 'final-project-romanov.appspot.com',
+    messagingSenderId: '554359325856',
+    appId: '1:554359325856:web:857423469e9c2b1fb435c5',
+    measurementId: 'G-L9BGMGNHV5'
+  }
 };
+
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
